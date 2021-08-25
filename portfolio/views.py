40317ae83e5bdc6675110d7040ed483656f74453
Bruno VIOLA <bruno.viola@protonmail.com>
2021-08-25 15:49:19 +0100

    adding home and about pages

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'portfolio/home.html')
+
+def about(request):
+    theauthor = 'bruvio'
+    return render(request,'portfolio/about.html',{'user':theauthor})
