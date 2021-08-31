fec14c66496ef929d0a4483c2d27f0dd4f0ba471
Bruno VIOLA <bruno.viola@protonmail.com>
2021-08-31 15:26:54 +0100

    limiting views to 5 blog

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from .models import Blog
 # Create your views here.
 def all_blogs(request):
 
-    all_blogs = Blog.objects.all()
+    all_blogs = Blog.objects.order_by('date')[:5]
     return render(request,'blog/all_blogs.html',{'all_blogs':all_blogs})
