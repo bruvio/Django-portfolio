09979347691f96be132771da0ece97b49537e1db
Bruno VIOLA <bruno.viola@protonmail.com>
2021-08-31 14:48:24 +0100

    adding blog page

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include, include
 from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
+
 urlpatterns = [
     path('',views.home,name='home'),
     path('about/',views.about,name='about'),
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+
 ]   
 
 urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
