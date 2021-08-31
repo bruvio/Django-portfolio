713f5a08707de23f3db29f03b8aed6f3bcb8a6e0
Bruno VIOLA <bruno.viola@protonmail.com>
2021-08-31 16:20:54 +0100

    viewing blog details

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 
 urlpatterns = [
     path("", views.all_blogs, name="all_blogs"),
+    path("<int:blog_id>/", views.detail, name="detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,8 @@ def all_blogs(request):
 
     all_blogs = Blog.objects.order_by("date")[:5]
     return render(request, "blog/all_blogs.html", {"all_blogs": all_blogs})
+
+
+def detail(request, blog_id):
+
+    return render(request, "blog/detail.html", {"id": blog_id})
