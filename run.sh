6fdefda822c3233e31d20b20756d44010cad17cc
Bruno VIOLA <bruno.viola@protonmail.com>
2021-09-06 15:52:10 +0100

    setting up run script for local development

--- run.sh
+++ run.sh
@@ -1,22 +1,27 @@
  #!/bin/bash
 
-echo "setup AWS enviromental variables "
-echo
-read -sp 'AWS_ACCESS_KEY_ID: ' key_id
-echo
-read -sp 'AWS_SECRET_ACCESS_KEY: ' access_key
-echo
-read -p 'AWS_STORAGE_BUCKET_NAME: ' bucketname
-echo
-read -p 'AWS_S3_REGION_NAME: ' region
-echo
+# echo "setup AWS enviromental variables "
+# echo
+# read -sp 'AWS_ACCESS_KEY_ID: ' key_id
+# echo
+# read -sp 'AWS_SECRET_ACCESS_KEY: ' access_key
+# echo
+# read -p 'AWS_STORAGE_BUCKET_NAME: ' bucketname
+# echo
+# read -p 'AWS_S3_REGION_NAME: ' region
+# echo
 
 
-export AWS_ACCESS_KEY_ID=${key_id}
-export AWS_SECRET_ACCESS_KEY=${access_key}
-export AWS_STORAGE_BUCKET_NAME=${bucketname}
-export AWS_S3_REGION_NAME=${region}
+# export AWS_ACCESS_KEY_ID=${key_id}
+# export AWS_SECRET_ACCESS_KEY=${access_key}
+# export AWS_STORAGE_BUCKET_NAME=${bucketname}
+# export AWS_S3_REGION_NAME=${region}
 
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsu
+python manage.py runserver
 
 #  eb init -p python-3.8 django-portfolio
 
