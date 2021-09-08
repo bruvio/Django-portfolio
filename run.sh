fa45bfb450a7fb3f8714248d2dc30e1dde9982af
Bruno VIOLA <bruno.viola@protonmail.com>
2021-09-08 09:11:29 +0100

    addding missing files to create s3 buckets

--- run.sh
+++ run.sh
@@ -1,16 +1,5 @@
  #!/bin/bash
 python create_s3bucket.py
-# echo
-# read -sp 'AWS_SECRET_ACCESS_KEY: ' access_key
-# echo
-python create_s3.py
-
-# read -p 'AWS_S3_REGION_NAME: ' region
-# echo
-# export AWS_ACCESS_KEY_ID=${key_id}
-# export AWS_SECRET_ACCESS_KEY=${access_key}
-# export AWS_STORAGE_BUCKET_NAME=${bucketname}
-# export AWS_S3_REGION_NAME=${region}
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsu
