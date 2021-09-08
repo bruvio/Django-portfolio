05a3e1a5560b79254e4544e90fd273cc6244fe95
Bruno VIOLA <bruno.viola@protonmail.com>
2021-09-08 08:59:11 +0100

    changed script for running locally

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
  #!/bin/bash
-
-# read -sp 'AWS_ACCESS_KEY_ID: ' key_id
+python create_s3bucket.py
 # echo
 # read -sp 'AWS_SECRET_ACCESS_KEY: ' access_key
 # echo
@@ -12,30 +11,7 @@ python create_s3.py
 # export AWS_SECRET_ACCESS_KEY=${access_key}
 # export AWS_STORAGE_BUCKET_NAME=${bucketname}
 # export AWS_S3_REGION_NAME=${region}
-
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsu
 python manage.py runserver
-
-# aws rds create-db-instance \
-# --db-instance-identifier portfolio \
-# --db-instance-class db.t3.micro \
-# --engine postgres \
-# --master-username postgres \
-# --master-user-password lorokpopen \
-# --allocated-storage 5
-
-
-# aws rds describe-db-instances --db-instance-identifier portfolio
-
-
-# aws rds delete-db-instance \
-# --db-instance-identifier portfolio \
-# --skip-final-snapshot
-
-#  eb init -p python-3.8 django-portfolio
-
-#  eb create django-env
-
-#  eb status
