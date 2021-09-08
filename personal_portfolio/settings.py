2a468d4b83f0efc22dd64e8c3f8548e3e8479b86
Bruno VIOLA <bruno.viola@protonmail.com>
2021-09-08 13:47:46 +0100

    removing db from config

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -156,3 +156,7 @@ except ImportError:
             "PORT": os.environ["RDS_PORT"],
         }
     }
+    AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME")
