a5b4d88daf190f757ff428ec41ad57a24744fa0c
bruvio <bruno.viola@pm.me>
2022-11-08 07:33:22 +0000

    remove static media config

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 python create_s3bucket.py
 
 eb init
-eb create portfolio1 -db.engine postgres --database.version 12
+eb create portfolio1 -db.engine postgres --database.version 12 --database.username postgresadmin --database.password "hAfF&U'9=YPNId)A0;2z"
