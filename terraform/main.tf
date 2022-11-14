11f022b5de73097ca224850da0a968af7e688ef6
bruvio <bruno.viola@pm.me>
2022-11-14 05:10:19 +0000

    back to default branch

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ data "aws_caller_identity" "current" {}
 
 
 module "personal_portfolio" {
-  source            = "git@github.com:bruvio/tf_fargate_django.git?ref=feature/reduce-costs"
+  source            = "git@github.com:bruvio/tf_fargate_django.git"
   project           = local.project
   admin             = var.admin
   table_name        = var.table_name
